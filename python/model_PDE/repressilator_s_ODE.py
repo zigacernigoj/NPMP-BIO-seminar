7019b25ef7d24f4d05996258834308e9897414ae
ziga <zc6857@student.uni-lj.si>
2018-12-12 12:45:47 +0100

    declare variables

--- python/model_PDE/repressilator_s_ODE.py
+++ python/model_PDE/repressilator_s_ODE.py
@@ -1,1 +1,16 @@
-# TODO
+# function  [dmA, dmB, dmC, dA, dB, dC, dS_i, dS_e] 
+# =
+# repressilator(
+#    CELLS, mA, mB, mC, A, B, C, S_i, S_e, 
+#    alpha, alpha0, Kd, beta, delta_m, delta_p, 
+#    n, kS0, kS1, kSe, kappa, eta)
+
+def repressilator_S_ODE(CELLS, mA, mB, mC, A, B, C, S_i, S_e, alpha, alpha0, Kd, beta, delta_m, delta_p, n, kS0, kS1, kSe, kappa, eta):
+    # TODO: rewrite from matlab file
+    print("repressilator_S_ODE")
+
+
+# test data:
+# TODO: define the values for variables
+# if __name__ == "__main__":
+#     repressilator_S_ODE()
