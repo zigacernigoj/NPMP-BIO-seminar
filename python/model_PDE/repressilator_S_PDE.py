55a374c76e4a0c21f3c26cc944a02ea577fc6aa5
Žiga Černigoj <zc6857@student.uni-lj.si>
2019-01-06 23:16:13 +0100

    put everything inside a function so you can create helper functions

--- python/model_PDE/repressilator_S_PDE.py
+++ python/model_PDE/repressilator_S_PDE.py
@@ -6,237 +6,257 @@ import scipy
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
-
 import time
 
-start_time = time.time()
 
-# ali rob predstavlja konec prostora ali so meje neskončne?
-# periodic_bounds = 1
-# nalaganje shranjene konfiguracije?
-# load_conf = 0
-# shranjevanje končne konfiguracije?
-# save_conf = 0
-# fiksni robovi ali spremenljivi
-# borderfixed = 0
-# snemanje videa - časovno potratno
-# movie_on = 0
+def shift_right(arr):
+    result = np.empty_like(arr)
+    result[:, 1:] = arr[:, :-1]
+    result[:, 0] = arr[:, -1]
+    return result
 
-# nalaganje vrednosti parametrov
-p = params = Parameters()
 
-# #### fiksirani parametri #### #
-# velikost stranice kvadrata, ki predstavlja svet
-size = p.size
+def shift_left(arr):
+    result = np.empty_like(arr)
+    result[:, :-1] = arr[:, 1:]
+    result[:, -1] = arr[:, 0]
+    return result
 
-# gostota "zivih" celic
-density = p.density
 
-# number of cells to be added to CELLS matrix
-n_cells = math.ceil(density * size**2)
+def shift_up(arr):
+    result = np.empty_like(arr)
+    result[:-1, :] = arr[1:, :]
+    result[-1, :] = arr[0, :]
+    return result
 
-# diffusion rate (hitrost sirjenja)
-D1 = p.D1
 
-# # environment # #
-# stevilo casovnih ciklov
-t_end = p.t_end
+def shift_down(arr):
+    result = np.empty_like(arr)
+    result[1:, :] = arr[:-1, :]
+    result[0, :] = arr[-1, :]
+    return result
 
-# velikost casovnega cikla
-dt = p.dt
 
-# Grid size: in micro meters - E coli size 1 um x 2 um (volume = 1 um^3)
-h = p.h
-h2 = p.h2
-# ######## #
+def simulate():
 
-# #### te parametre je treba pogruntat z optimizac. algoritmi #### #
-alpha = p.alpha
-alpha0 = p.alpha0
-Kd = p.Kd
-delta_m = p.delta_m
-delta_p = p.delta_p
-n = p.n
-beta = p.beta 
-kappa = p.kappa
-kS0 = p.kS0
-kS1 = p.kS1
-kSe = p.kSe
-eta = p.eta
-# ######## #
+    start_time = time.time()
 
-# #### matrike in ostale spremenljivke za delovanje modela #### #
+    # fix random seed for testing purposes
+    # np.random.seed(1)
 
-# S=zeros(size,size) #Initialise species S
-S_e = np.random.rand(size,size)
-S_i = np.zeros((size,size))
-A = np.zeros((size,size))
-B = np.zeros((size,size))
-C = np.zeros((size,size))
-mA = np.zeros((size,size))
-mB = np.zeros((size,size))
-mC = np.zeros((size,size))
+    # ali rob predstavlja konec prostora ali so meje neskončne?
+    # periodic_bounds = 1
+    # nalaganje shranjene konfiguracije?
+    # load_conf = 0
+    # shranjevanje končne konfiguracije?
+    # save_conf = 0
+    # fiksni robovi ali spremenljivi
+    # borderfixed = 0
+    # snemanje videa - časovno potratno
+    # movie_on = 0
 
-# this creates a matrix (CELLS) with randomly located cells
-# number of cells = n_cells
-# 1 represents a cell, 0 represents an empty place
-# indices of cells are in cell_idx (1D) 
-# and cell_matrix_idx (2D)
+    # nalaganje vrednosti parametrov
+    p = params = Parameters()
 
-# flattened 10*10 matrix = array with 100 elements 
-nums = np.zeros(100)
+    # #### fiksirani parametri #### #
+    # velikost stranice kvadrata, ki predstavlja svet
+    size = p.size
 
-# some places contain cells (number of cells = n_cells)
-nums[:n_cells] = 1
+    # gostota "zivih" celic
+    density = p.density
 
-# shuffle the array
-np.random.shuffle(nums)
+    # number of cells to be added to CELLS matrix
+    n_cells = math.ceil(density * size**2)
 
-# reshape array into the matrix
-CELLS = np.reshape(nums, (size, size))
+    # diffusion rate (hitrost sirjenja)
+    D1 = p.D1
 
-# locations of cells in 1D array 
-cell_idx = np.argwhere(nums == 1)
+    # # environment # #
+    # stevilo casovnih ciklov
+    t_end = p.t_end
 
-# locations of cells in 2D matrix, already sorted
-cell_matrix_idx = np.argwhere(CELLS == 1)
+    # velikost casovnega cikla
+    dt = p.dt
 
-first_idx = cell_idx[0]
-first_matrix_idx = cell_matrix_idx[0]
+    # Grid size: in micro meters - E coli size 1 um x 2 um (volume = 1 um^3)
+    h = p.h
+    h2 = p.h2
+    # ######## #
 
-for (x, y) in cell_matrix_idx:
-    A[x, y] = 100*np.random.rand()
-    mA[x, y] = 100*np.random.rand()
-    B[x, y] = 100*np.random.rand()
-    mB[x, y] = 100*np.random.rand()
-    C[x, y] = 100*np.random.rand()
-    mC[x, y] = 100*np.random.rand()
+    # #### te parametre je treba pogruntat z optimizac. algoritmi #### #
+    alpha = p.alpha
+    alpha0 = p.alpha0
+    Kd = p.Kd
+    delta_m = p.delta_m
+    delta_p = p.delta_p
+    n = p.n
+    beta = p.beta
+    kappa = p.kappa
+    kS0 = p.kS0
+    kS1 = p.kS1
+    kSe = p.kSe
+    eta = p.eta
+    # ######## #
 
-A_series = np.zeros((1, int(t_end/dt)))
-S_e_series = np.zeros((1, int(t_end/dt)))
-A_full = np.zeros((int(t_end/dt), n_cells))
+    # #### matrike in ostale spremenljivke za delovanje modela #### #
 
-A_series[0, 0] = A[first_matrix_idx[0], first_matrix_idx[1]]
-S_e_series[0, 0] = S_e[first_matrix_idx[0], first_matrix_idx[1]]
-A_full[0, :] = A[cell_matrix_idx[:, 0], cell_matrix_idx[:, 1]]
+    # S=zeros(size,size) #Initialise species S
+    S_e = np.random.rand(size,size)
+    S_i = np.zeros((size,size))
+    A = np.zeros((size,size))
+    B = np.zeros((size,size))
+    C = np.zeros((size,size))
+    mA = np.zeros((size,size))
+    mB = np.zeros((size,size))
+    mC = np.zeros((size,size))
 
-# TODO: LOAD CONFIGURATION
+    # this creates a matrix (CELLS) with randomly located cells
+    # number of cells = n_cells
+    # 1 represents a cell, 0 represents an empty place
+    # indices of cells are in cell_idx (1D)
+    # and cell_matrix_idx (2D)
 
-t = 0
-k = 0
-step = 0
+    # flattened 10*10 matrix = array with 100 elements
+    nums = np.zeros(100)
 
-setup_time_sum = 0
-integr_time_sum = 0
-mul_time_sum = 0
-other_time_sum = 0
+    # some places contain cells (number of cells = n_cells)
+    nums[:n_cells] = 1
 
-while t < t_end:
-    # print("t:", t)
-    # print("step:", step)
+    # shuffle the array
+    np.random.shuffle(nums)
 
-    setup_start = time.time()
+    # reshape array into the matrix
+    CELLS = np.reshape(nums, (size, size))
 
-    first_col = S_e[:, 0][np.newaxis].T
-    after_cols = S_e[:, 1:]
+    # locations of cells in 1D array
+    cell_idx = np.argwhere(nums == 1)
 
-    last_col = S_e[:, -1][np.newaxis].T
-    before_cols = S_e[:, :-1]
+    # locations of cells in 2D matrix, already sorted
+    cell_matrix_idx = np.argwhere(CELLS == 1)
 
-    first_row = S_e[0, :][np.newaxis]
-    after_rows = S_e[1:, :]
+    first_idx = cell_idx[0]
+    first_matrix_idx = cell_matrix_idx[0]
 
-    last_row = S_e[-1, :][np.newaxis]
-    before_rows = S_e[:-1, :]
+    for (x, y) in cell_matrix_idx:
+        A[x, y] = 100*np.random.rand()
+        mA[x, y] = 100*np.random.rand()
+        B[x, y] = 100*np.random.rand()
+        mB[x, y] = 100*np.random.rand()
+        C[x, y] = 100*np.random.rand()
+        mC[x, y] = 100*np.random.rand()
 
-    two_times_Se = 2 * S_e
+    A_series = np.zeros((1, int(t_end/dt)))
+    S_e_series = np.zeros((1, int(t_end/dt)))
+    A_full = np.zeros((int(t_end/dt), n_cells))
 
-    S_e_xx = D1 * (np.append(last_col, before_cols, axis=1) + np.append(after_cols, first_col, axis=1) - two_times_Se) / h2
-    S_e_yy = D1 * (np.append(last_row, before_rows, axis=0) + np.append(after_rows, first_row, axis=0) - two_times_Se) / h2
+    A_series[0, 0] = A[first_matrix_idx[0], first_matrix_idx[1]]
+    S_e_series[0, 0] = S_e[first_matrix_idx[0], first_matrix_idx[1]]
+    A_full[0, :] = A[cell_matrix_idx[:, 0], cell_matrix_idx[:, 1]]
 
-    setup_time_sum += time.time() - setup_start
+    # TODO: LOAD CONFIGURATION
 
-    D2S_e = S_e_xx + S_e_yy
+    t = 0
+    k = 0
+    step = 0
 
-    # Calculate dx/dt
-    integr_start = time.time()
-    [dmA, dmB, dmC, dA, dB, dC, dS_i, dS_e] = repressilator_S_ODE(CELLS, mA, mB, mC, A, B, C, S_i, S_e, alpha, alpha0, Kd, beta, delta_m, delta_p, n, kS0, kS1, kSe, kappa, eta)
-    integr_time_sum += time.time() - integr_start
+    setup_time_sum = 0
+    integr_time_sum = 0
+    mul_time_sum = 0
+    other_time_sum = 0
 
-    other_start = time.time()
-    dS_e = dS_e + D2S_e
-    other_time_sum += time.time() - other_start
+    while t < t_end:
+        # print("t:", t)
+        # print("step:", step)
 
-    mul_start = time.time()
-    mA = mA + np.multiply(dt, dmA)
-    mB = mB + np.multiply(dt, dmB)
-    mC = mC + np.multiply(dt, dmC)
-    A = A + np.multiply(dt, dA)
-    B = B + np.multiply(dt, dB)
-    C = C + np.multiply(dt, dC)
-    S_i = S_i + np.multiply(dt, dS_i)
-    S_e = S_e + np.multiply(dt, dS_e)
-    mul_time_sum += time.time() - mul_start
+        setup_start = time.time()
 
-    other_start = time.time()
-    A_series[0, step] = A[first_matrix_idx[0], first_matrix_idx[1]]
-    S_e_series[0, step] = S_e[first_matrix_idx[0], first_matrix_idx[1]]
-    A_full[step,:] = A[cell_matrix_idx[:,0], cell_matrix_idx[:,1]]
+        two_times_Se = 2 * S_e
+        S_e_xx = D1 * (shift_right(S_e) + shift_left(S_e) - two_times_Se) / h2
+        S_e_yy = D1 * (shift_down(S_e) + shift_up(S_e) - two_times_Se) / h2
 
-    # increments AFTER ... see if it actually works
-    t += dt
-    step += 1
-    other_time_sum += time.time() - other_start
+        setup_time_sum += time.time() - setup_start
 
-# TODO: SAVE CONFIGURATION
+        D2S_e = S_e_xx + S_e_yy
 
-print("--- %s seconds ---" % (time.time() - start_time))
-print("--- %s seconds for setup ---" % setup_time_sum)
-print("--- %s seconds for integration ---" % integr_time_sum)
-print("--- %s seconds for mul ---" % mul_time_sum)
-print("--- %s seconds for other ---" % other_time_sum)
+        # Calculate dx/dt
+        integr_start = time.time()
+        [dmA, dmB, dmC, dA, dB, dC, dS_i, dS_e] = repressilator_S_ODE(CELLS, mA, mB, mC, A, B, C, S_i, S_e, alpha, alpha0, Kd, beta, delta_m, delta_p, n, kS0, kS1, kSe, kappa, eta)
+        integr_time_sum += time.time() - integr_start
 
-# GRAPHS
+        other_start = time.time()
+        dS_e = dS_e + D2S_e
+        other_time_sum += time.time() - other_start
 
-T = np.arange(0, t_end, dt)[np.newaxis]
+        mul_start = time.time()
+        mA = mA + np.multiply(dt, dmA)
+        mB = mB + np.multiply(dt, dmB)
+        mC = mC + np.multiply(dt, dmC)
+        A = A + np.multiply(dt, dA)
+        B = B + np.multiply(dt, dB)
+        C = C + np.multiply(dt, dC)
+        S_i = S_i + np.multiply(dt, dS_i)
+        S_e = S_e + np.multiply(dt, dS_e)
+        mul_time_sum += time.time() - mul_start
 
-# doesn't show data
-# plt.figure(1)
-# plt.plot(T, S_e_series)
-# plt.xlabel('Time [min]')
-# plt.ylabel('Concentration [nM]')
+        other_start = time.time()
+        A_series[0, step] = A[first_matrix_idx[0], first_matrix_idx[1]]
+        S_e_series[0, step] = S_e[first_matrix_idx[0], first_matrix_idx[1]]
+        A_full[step,:] = A[cell_matrix_idx[:,0], cell_matrix_idx[:,1]]
 
-# doesn't show data
-# plt.figure(2)
-# plt.plot(T,A_series, T, S_e_series)
-# plt.xlabel('Time [min]')
-# plt.ylabel('Concentration [nM]')
+        # increments AFTER ... see if it actually works
+        t += dt
+        step += 1
+        other_time_sum += time.time() - other_start
 
-TT = T.T
-TMat = np.repeat(TT, n_cells, 1)
+    # TODO: SAVE CONFIGURATION
 
-y = np.arange(0, n_cells)[np.newaxis]
-yT = y.T
-yMat = np.repeat(yT, TT.size, 1)
-yMat = yMat.T
+    print("--- %s seconds ---" % (time.time() - start_time))
+    print("--- %s seconds for setup ---" % setup_time_sum)
+    print("--- %s seconds for integration ---" % integr_time_sum)
+    print("--- %s seconds for mul ---" % mul_time_sum)
+    print("--- %s seconds for other ---" % other_time_sum)
 
-# graf prikazuje koncentracijo molekule v celicah skozi cas
-fig, ax1 = plt.subplots(subplot_kw={'projection': '3d'})
-ax1.plot_wireframe(TMat, yMat, A_full)
-ax1.set_title("concentration in cells through time")
+    # GRAPHS
 
-# graf prikazuje razporeditev celic
-plt.figure(4)
-plt.imshow(CELLS, cmap='binary')
-plt.xlabel(r'$\mu m$')
-plt.xticks(np.arange(0, size), np.arange(0, size/2, step=0.5))
-plt.ylabel(r'$\mu m$')
-plt.yticks(np.arange(0, size), np.arange(0, size/2, step=0.5))
+    T = np.arange(0, t_end, dt)[np.newaxis]
 
-# za vse plote prikazat
-plt.show()
+    # doesn't show data
+    # plt.figure(1)
+    # plt.plot(T, S_e_series)
+    # plt.xlabel('Time [min]')
+    # plt.ylabel('Concentration [nM]')
+
+    # doesn't show data
+    # plt.figure(2)
+    # plt.plot(T,A_series, T, S_e_series)
+    # plt.xlabel('Time [min]')
+    # plt.ylabel('Concentration [nM]')
+
+    TT = T.T
+    TMat = np.repeat(TT, n_cells, 1)
+
+    y = np.arange(0, n_cells)[np.newaxis]
+    yT = y.T
+    yMat = np.repeat(yT, TT.size, 1)
+    yMat = yMat.T
+
+    # graf prikazuje koncentracijo molekule v celicah skozi cas
+    fig, ax1 = plt.subplots(subplot_kw={'projection': '3d'})
+    ax1.plot_wireframe(TMat, yMat, A_full)
+    ax1.set_title("concentration in cells through time")
+
+    # graf prikazuje razporeditev celic
+    plt.figure(4)
+    plt.imshow(CELLS, cmap='binary')
+    plt.xlabel(r'$\mu m$')
+    plt.xticks(np.arange(0, size), np.arange(0, size/2, step=0.5))
+    plt.ylabel(r'$\mu m$')
+    plt.yticks(np.arange(0, size), np.arange(0, size/2, step=0.5))
+
+    # za vse plote prikazat
+    plt.show()
+
 
 if __name__ == "__main__":
-    # params = Parameters()
-    # print(params.size)
-    print("hello world")
+    print("starting simulation")
+    simulate()
